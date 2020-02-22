e9b01700ca03615af6708e4cc65e1048b9e434df
M. Minskis <martynas.minskis@gmail.com>
2020-02-22 16:47:02 +0000

    Add Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -185,4 +185,69 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      // Need a validation for a title, company and a from-date
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', '"From" date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // If there are errors
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    // Creating Obj. with the data that user SUBMITS
+    // Instead of eg. "title: title" we use just "title"
+    const newExperience = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      // Fetching the profile to whitch we want to add a "newExperience"
+      // Recap: "req.user.id" comes from the token
+      const profile = await Profile.findOne({ user: req.user.id });
+      // "unshift()" pushes data to the beginning of array
+      profile.experience.unshift(newExperience);
+      // Saving to DB
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
