39b0b834e5099c8f3cedada896b263cbb6c06059
M. Minskis <martynas.minskis@gmail.com>
2020-03-06 13:14:34 +0000

    Add React Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 // Fragment is a "gost" element it will not show up in the DOM
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  // Need to wrap everything within Router
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      {/* Every page within the theme (except for the landing page) has a className="container" to push the content to the middle */}
+      <section className='container'>
+        {/* Wrapping everything in the Switch */}
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
