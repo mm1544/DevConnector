833f22018cf62db7105a7f917907ac56cb8277f9
M. Minskis <martynas.minskis@gmail.com>
2020-03-31 10:19:21 +0100

    Add AddEducation

---
+++ client/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,146 @@
+// React Arrow Function Component w/ PropTypes (racfp)
+
+import React, { Fragment, useState } from 'react';
+// 'withRouter' is to redirect in action file...
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEducation } from '../../actions/profile';
+
+const AddEducation = ({ addEducation, history }) => {
+  const [formData, setFormData] = useState({
+    school: '',
+    degree: '',
+    fieldofstudy: '',
+    from: '',
+    // 'to' has to be disabled if 'current' is true
+    to: '',
+    current: false,
+    description: ''
+  });
+
+  // State for 'to' state ('to' has to be disabled if 'current' is true)
+  const [toDateDisabled, toggleDisabled] = useState(false);
+
+  // Destructuring formData
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = formData;
+
+  // Takes the Ket 'e.target.name' and assigns to it the Value 'e.target.value' (and adds to the component(?) state)
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Add Your Education</h1>
+      <p className='lead'>
+        <i className='fas fa-code-branch'></i> Add any school or bootcamp that
+        you have attended
+      </p>
+      <small>* = required field</small>
+      <form
+        className='form'
+        onSubmit={e => {
+          e.preventDefault();
+          // 'history' obj. is in props
+          addEducation(formData, history);
+        }}
+      >
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* School or Bootcamp'
+            name='school'
+            value={school}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Degree or Certificate'
+            name='degree'
+            value={degree}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Field of Study'
+            name='fieldofstudy'
+            value={fieldofstudy}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <h4>From Date</h4>
+          <input
+            type='date'
+            name='from'
+            value={from}
+            onChange={e => onChange(e)}
+          />
+        </div>
+
+        <div className='form-group'>
+          <p>
+            <input
+              type='checkbox'
+              name='current'
+              checked={current}
+              value={current}
+              // Toggling the checkbox and adding it's state to the formData state
+              onChange={e => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+            />{' '}
+            Currently Studying
+          </p>
+        </div>
+
+        <div className='form-group'>
+          <h4>To Date</h4>
+          <input
+            type='date'
+            name='to'
+            value={to}
+            onChange={e => onChange(e)}
+            disabled={toDateDisabled ? 'disabled' : ''}
+          />
+        </div>
+
+        <div className='form-group'>
+          <textarea
+            name='description'
+            cols='30'
+            rows='5'
+            placeholder='Program Description'
+            value={description}
+            onChange={e => onChange(e)}
+          ></textarea>
+        </div>
+        <input type='submit' className='btn btn-primary my-1' />
+        <a className='btn my-1' href='dashboard.html'>
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
+};
+
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired
+};
+
+export default connect(null, { addEducation })(AddEducation);
