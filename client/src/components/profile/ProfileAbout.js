06cf94ec33227f6d0ebba374d0fdb1c509112376
M. Minskis <martynas.minskis@gmail.com>
2020-04-02 12:22:49 +0100

    Add ProfileExperience.js and ProfileEducation.js

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -9,20 +9,20 @@ const ProfileAbout = ({
     user: { name }
   }
 }) => (
-  <div class='profile-about bg-light p-2'>
+  <div className='profile-about bg-light p-2'>
     {/* Because 'bio' field entry is optional */}
     {bio && (
       <Fragment>
         {/* 'name.trim().split(' ')[0]' is to extract the name */}
-        <h2 class='text-primary'>Bio of {name.trim().split(' ')[0]}</h2>
+        <h2 className='text-primary'>Bio of {name.trim().split(' ')[0]}</h2>
         <p>{bio}</p>
         {/* <!-- Creates a line --> */}
-        <div class='line'></div>
+        <div className='line'></div>
       </Fragment>
     )}
 
-    <h2 class='text-primary'>Skill Set</h2>
-    <div class='skills'>
+    <h2 className='text-primary'>Skill Set</h2>
+    <div className='skills'>
       {skills.map((skill, index) => (
         <div key={index} className='p-1'>
           <i className='fas fa-check'></i> {skill}
