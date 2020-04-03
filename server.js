b17d7d66780a411355dd5a39c63101ae53c731d1
M. Minskis <martynas.minskis@gmail.com>
2020-04-03 19:48:09 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+// For deployment
+const path = require('path');
 // Initialising app variable for express
 const app = express();
 // Connect to the database
@@ -9,8 +11,8 @@ connectDB();
 // To be able to use eg. "req.body" need to initialise a middleware for the body parser. Parser is included with Express.
 app.use(express.json({ extended: false }));
 
-// endpoint for testing
-app.get('/', (req, res) => res.send('API Running'));
+// endpoint for testing (commenting-out for deployment)
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 // It will pertain "/api/users" to '/'. I.e. if we get API request to "/api/users", it will get users.js
@@ -19,7 +21,18 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Setting Port tobe an environmental variable. If there is no env var, it will use port 5000
+// Serve static assets in production (for deployment)
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // To serve index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Setting Port to be an environmental variable. If there is no env var, it will use port 5000
 const PORT = process.env.PORT || 5000;
 // setting to listen port with callback fn which will execute once app will connect
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
